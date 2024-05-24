42a790ffb814f7af6cb3cdbc5424e2a2e8527dab
bgruehn <benjamin.gruehn@gmail.com>
2024-05-24 12:05:43 +0200

    Final New

--- src/main.ts
+++ src/main.ts
@@ -21,3 +21,58 @@ let mindestensEineGroesserAls10 = zahlenArray3.some(function(zahl) {
     return zahl > 10;
 });
 console.log(mindestensEineGroesserAls10);
+
+const person: { name: string; alter?: number } = {
+    name: "Hans",
+    alter: 30
+};
+
+console.log(person);
+
+
+
+type Grade = number | string;
+
+function printStudentGrades(firstName: string, lastName: string, age: number, grades: Grade[]): void {
+    console.log(firstName + " " + lastName + " (" + age + ")");
+    console.log("=".repeat(30));
+
+    const gradesString = grades.map(grade => {
+        if (grade === undefined) {
+            return "*";
+        } else if (typeof grade === "string") {
+            return grade;
+        } else if (grade === 1) {
+            return "A";
+        } else if (grade === 2) {
+            return "B";
+        } else if (grade === 3) {
+            return "C";
+        } else if (grade === 4) {
+            return "D";
+        } else if (grade === 5) {
+            return "E";
+        } else if (grade === 6) {
+            return "F";
+        }
+
+        return grade.toString();
+    }).join(",");
+
+    console.log("Noten: " + gradesString);
+}
+
+const students = [
+    { firstName: "Bernd", lastName: "Meier", age: 17, grades: [1, 4, 3, 1, "D", "A", 1, 2] },
+    { firstName: "Uwe", lastName: "Müller", age: 18, grades: ["A", "B", 1] },
+    { firstName: "Hans", lastName: "Schmidt", age: 17, grades: ["B", 1, "C", 3, 2, 4, 5] }
+];
+
+function printAllStudents(): void {
+    students.forEach(student => {
+        printStudentGrades(student.firstName, student.lastName, student.age, student.grades);
+        console.log();
+    });
+}
+
+printAllStudents();
